37560e6e77d2fb4414ef7124324c7c45b826a1e2
Steven Saunders <stevensaunders@Stevens-MacBook-Pro.local>
2019-12-31 16:32:54 -0700

    added client side validation with JS, working out some issues with error styling

---
+++ app.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(`form`);
+const emailInput = document.querySelector(`input`);
+const errorMessage = document.querySelector(`#email-validate`);
+
+form.addEventListener(`submit`, (e) => {
+	e.preventDefault();
+	checkEmail();
+})
+
+//CHECK EMAIL AGAINST INVALID INPUTS
+function checkEmail() {
+	const emailValue = emailInput.value.trim();
+	if(emailValue === '' || emailValue !== validateEmail(emailValue)) {
+		setErrorFor();
+	}
+}
+
+//REGEX EMAIL VALIDATION
+function validateEmail(email) {
+	const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+  return expression.test(String(email).toLowerCase())
+}
+
+//SETTING THE CLASSES TO SHOW ERROR
+function setErrorFor() {
+	emailInput.classList.add(`error-border`);
+	errorMessage.classList.remove(`hidden`);
+}
