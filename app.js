d58aa4a779a4c8c790c26fb984eb19f006c27bf0
Steven Saunders <stevensaunders@Stevens-MacBook-Pro.local>
2020-01-08 16:53:01 -0700

    Fixed image sizes in mobile and desktop, re-wrote css and added JS to swap images for window widths

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const form = document.querySelector(`form`);
 const emailInput = document.querySelector(`#email`);
 const errorMessage = document.querySelector(`#email-reject-text`);
 const errorImg = document.querySelector(`#email-reject-img`);
+const imageToggle = document.querySelector(`#hero-img`);
+const windowWidth = window.innerWidth;
 
 const setError = () => {
 	emailInput.classList.add(`red-border`);
-	emailInput.classList.remove(`green-border`);
 	errorMessage.classList.remove(`hidden`);
 	errorImg.classList.remove(`hidden`);
 }
 
 const setSuccess = () => {
 	emailInput.classList.remove(`red-border`);
-	emailInput.classList.add(`green-border`);
 	errorMessage.classList.add(`hidden`);
 	errorImg.classList.add(`hidden`);
 }
@@ -21,6 +21,8 @@ form.addEventListener(`submit`, (e) => {
 	e.preventDefault();
 	checkEmail();
 })
+
+window.addEventListener(`resize`, swapImage());
 
 function checkEmail() {
 	const emailValue = emailInput.value.trim();
@@ -34,4 +36,12 @@ function checkEmail() {
 function validateEmail(email) {
 	const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 	return expression.test(String(email).toLowerCase())
+}
+
+function swapImage() {
+	if(windowWidth >= 750) {
+		imageToggle.
+	}	else {
+
+	}
 }
